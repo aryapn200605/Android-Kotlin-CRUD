d8db812e259cffb5aec68dd87ab21c5a61f91ec4
ARYA <aryapn200605@gmail.com>
2023-02-28 14:02:09 +0700

    db helper

--- app/src/main/java/com/example/crudapp/BottomActivity.kt
+++ app/src/main/java/com/example/crudapp/BottomActivity.kt
@@ -21,7 +21,7 @@ class BottomActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_bottom)
+        val navController = findNavController(R.id.nav_view)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
